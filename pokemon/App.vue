01b34cfd83b8c2b5f1fc84ec4dd44d68f55c08c0
Maxim Klochkov <m.klochkov@sap.com>
2020-11-17 21:26:41 +0300

    Starting Pokemn API project

---
+++ pokemon/App.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="cards">
+        <div class="card" v-for="starter in starters">
+            <div class="title">
+                {{ starter.name}}
+            </div>
+            <div class="content">
+                <img :src="starter.sprite">
+            </div>
+            <div class="description">
+                <div v-for="type in starter.types">{{ type }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+const api = 'https://pokeapi.co/api/v2/pokemon';
+const ids = [1, 4, 7];
+
+export default {
+    created() {
+        this.fetchData();
+    },
+
+    data() {
+        return {
+            starters: null
+        }
+    },
+
+    methods: {
+        async fetchData() {
+            const responses = await Promise.all(ids.map(id => fetch(`${api}/${id}`)));
+            const data = await Promise.all(responses.map(response => response.json()));
+            this.starters = data.map(datum => ({
+                name: datum.name,
+                sprite: datum.sprites.other['official-artwork'].front_default,
+                types: datum.types.map(type => type.type.name)
+            }));
+            console.log(this.starters);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.cards {
+    display: flex;
+}
+
+.card {
+    border: 1px solid silver;
+    border-radius: 8px;
+    max-width: 200px;
+    margin: 0 5px;
+    cursor: pointer;
+    box-shadow: 0px 1px 3px darkgrey;
+    transition: 0.2s;
+}
+
+img {
+    max-width: 100%;
+}
+
+.title, .content, .description {
+    padding: 16px;
+    text-transform: capitalize;
+    text-align: center;
+}
+
+.title, .content {
+    border-bottom: 1px solid silver;
+}
+
+.title {
+    font-size: 1.25em;
+}
+
+.card:hover {
+    transition: 0.2s;
+    box-shadow: 0px 1px 9px darkgrey;
+}
+</style>
