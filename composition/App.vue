650798f0aca4f3e5d40fb276205d49a17620e886
Maxim Klochkov <m.klochkov@sap.com>
2020-11-19 16:32:34 +0300

    Composition API basics added

---
+++ composition/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <button @click="increment">
+        {{ count }}
+    </button>
+    <button @click="a++">
+        {{ a }}
+    </button>
+    <button @click="b++">
+        {{ b }}
+    </button>
+    <ul>
+        <li v-for="value in history">
+            {{ value }}
+        </li>
+    </ul>
+</template>
+
+<script>
+import { useNumbers } from './numbers'
+
+export default {
+    setup() {
+        const {
+            a,
+            b,
+            count,
+            history,
+            increment
+        } = useNumbers()
+
+        return {
+            a,
+            b,
+            count,
+            history,
+            increment
+        }
+    }
+}
+</script>
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
-import App from './pokemon/App.vue';
+// import App from './pokemon/App.vue';
+import CompositionApp from './composition/App.vue';
 
-createApp(App).mount('#app');
+createApp(CompositionApp).mount('#app');
